f7b120aaa4d1f673893c376105f6f9e69a6537d8
jwclark <jwclark@rockhursths.edu>
2016-05-23 14:49:27 -0500

    successfully split the api to HTTPS without redirect

--- server.js
+++ server.js
@@ -7,15 +7,17 @@ var fs = require('fs');
 var http = require('http');
 var https = require('https');
 var vhost = require('vhost');
+var helmet = require('helmet');
 var express = require('express');
 
 var app = express();
 
+var DOMAIN = 'local.info';
 var API_DOMAIN = 'sub.local.info';
 
 var credentials = {
-    key: fs.readFileSync('bin/dev-key.pem'),
-    cert: fs.readFileSync('bin/dev-cert.pem')
+    key: fs.readFileSync('bin/key.pem'),
+    cert: fs.readFileSync('bin/cert.pem')
 };
 
 var log = function(msg, obj) {
@@ -43,16 +45,29 @@ var log = function(msg, obj) {
     }
 };
 
-app.use('/', express.static(__dirname + '/site'));
+app.use(helmet.noCache());
+app.use(helmet.frameguard());
+app.use(helmet.noSniff());
+app.use(helmet.hsts({
+    maxAge: 2592000000, // 30 days in milliseconds
+    includeSubdomains: true
+}));
 
+app.use('/', express.static(__dirname + '/site'));
 app.use(vhost(API_DOMAIN, require('./api.js').app));
 
 https.createServer(credentials, app).listen(443);
 
 http.createServer(function(req, res) {
-    log('http redirect to https');
-	res.writeHead(301, {'Location':'https://' + req.headers['host'] + req.url });
-	res.end();
+    if(req.host === DOMAIN) {
+        log('redirecting home');
+        res.writeHead(301, {'Location':'https://' + req.headers.host + req.url });
+        res.end();
+    } else {
+        log('forbidden caught');
+        res.writeHead(403);
+        res.end();
+    }
 }).listen(80);
 
 log('ready to serve');
